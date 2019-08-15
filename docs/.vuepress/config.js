7c8bcf2ef60242497fca04a02f1eff12ab31b79c
jack-94 <1213845080@qq.com>
2019-08-15 15:02:55 +0800

    新建deploy.sh文件，部署在https://jack-94.github.io/vuepress-blog/

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,23 @@
 module.exports = {
   title: '健强的博客小站',
   description: 'Just playing around',
+  locales: {
+    '/': {
+      lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+    }
+  },
   head: [ // 注入到当前页面的 HTML <head> 中的标签
     ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
-  base: '/', // 这是部署到github相关的配置
+  // base: '/blog/', // 这是部署到github相关的配置
   markdown: {
     lineNumbers: false // 代码块显示行号
   },
   themeConfig: {
-    lastUpdated: 'Last Updated',
+    repo: 'jack-94/vuepress-blog',
+    editLinks: true,
+    editLinkText: '编辑文档',
+    lastUpdated: '上次更新',
     nav:[ // 导航栏配置
       {
         text: '牛逼',
--- docs/about/README.md
+++ docs/about/README.md
@@ -1,4 +1,4 @@
-# ABOUT首页
+# ABOUT首页 <Badge text="beta" type="warn"/> <Badge text="0.10.1+"/> <Badge text="默认主题"/>
 
 
 
